c73458ae042d362213ca89b92bbac196838a0db8
ahmedmami123 <ahmedmami940@gmail.com>
2025-10-21 21:55:58 +0100

    New update available

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import RegisterLogin from './Pages/RegisterLogin';
 import Profile from './Pages/Profile';
 import VerifyAccount from './Pages/VerifyAccount';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ResetPassword from './Pages/ResetPassword';
 import OAuthCallback from './Pages/OAuthCallback';
@@ -12,9 +12,31 @@ import OAuthCallback from './Pages/OAuthCallback';
 function App() {
   const { user } = useSelector(state => state.auth);
 
+  // ----------------- Auto-update check -----------------
+  useEffect(() => {
+    async function checkUpdate() {
+      try {
+        const res = await fetch('https://github.com/ahmedmami123/tauri.git');
+        const data = await res.json();
+
+        const lastHash = localStorage.getItem('lastUpdateHash');
+
+        if (lastHash !== data.hash) {
+          toast.info(`Update available: ${data.message}`, { autoClose: 5000 });
+          localStorage.setItem('lastUpdateHash', data.hash);
+        }
+      } catch (err) {
+        console.error("Failed to check update:", err);
+      }
+    }
+
+    checkUpdate();
+  }, []);
+  // ------------------------------------------------------
+
   return (
     <div>
-  <Routes>
+      <Routes>
         <Route 
           path="/" 
           element={
@@ -38,9 +60,8 @@ function App() {
           } 
         />
         <Route path="*" element={<Navigate to="/" />} />
-         <Route path="/reset-password" element={<ResetPassword />} />
-                 <Route path="/oauth-callback" element={<OAuthCallback />} />
-
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/oauth-callback" element={<OAuthCallback />} />
       </Routes>
       <ToastContainer position="top-right" autoClose={3000} />
     </div>
